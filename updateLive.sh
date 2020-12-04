2ee8672a7bc4542e33cb23784a581b326c2fe87f
Justin Guese <guese.justin@gmail.com>
2020-12-04 19:24:40 +0100

    auto update to codebase

--- updateLive.sh
+++ updateLive.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 hugo
-git add -A
-git commit -m "auto update to codebase"
-git push origin master
-echo "done"
 cd public
 git add -A 
 git commit -m "update to the live website"
 git push origin master
-
+cd ../
+git add -A
+git commit -m "auto update to codebase"
+git push origin master
+echo "done"
