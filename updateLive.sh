0e818ae9ca27aa52be76dc498b69e6b6cb5f9463
Justin Thinkpad <guese.justin@gmail.com>
2020-12-16 12:18:58 +0100

    auto update to codebase

--- updateLive.sh
+++ updateLive.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 hugo
 cd public
-git add -A 
-git commit -m "update to the live website"
-git push origin master
+git -C /home/justin/docker/datafortress.cloud.hugo/public add -A 
+git -C /home/justin/docker/datafortress.cloud.hugo/public commit -m "update to the live website"
+git -C /home/justin/docker/datafortress.cloud.hugo/public push origin master
 cd ../
-git add -A
-git commit -m "auto update to codebase"
-git push origin master
+git -C /home/justin/docker/datafortress.cloud.hugo add -A
+git -C /home/justin/docker/datafortress.cloud.hugo commit -m "auto update to codebase"
+git -C /home/justin/docker/datafortress.cloud.hugo push origin master
 echo "done"
