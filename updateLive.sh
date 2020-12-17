3791bd4b2741beb5299f1743d654f430cc17a9b5
Justin Thinkpad <guese.justin@gmail.com>
2020-12-17 12:18:17 +0100

    auto update to codebase

--- updateLive.sh
+++ updateLive.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 (cd /home/justin/docker/datafortress.cloud.hugo ; hugo)
-cd public
 git -C /home/justin/docker/datafortress.cloud.hugo/public add -A 
 git -C /home/justin/docker/datafortress.cloud.hugo/public commit -m "update to the live website"
 git -C /home/justin/docker/datafortress.cloud.hugo/public push origin master
-cd ../
 git -C /home/justin/docker/datafortress.cloud.hugo add -A
 git -C /home/justin/docker/datafortress.cloud.hugo commit -m "auto update to codebase"
 git -C /home/justin/docker/datafortress.cloud.hugo push origin master
