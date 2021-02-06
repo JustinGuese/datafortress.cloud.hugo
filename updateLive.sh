7b43f49c0f9188d620c2f928f0f4bf142298d6b5
Justin Guese <guese.justin@gmail.com>
2021-02-06 12:16:45 +0100

    change folder

--- updateLive.sh
+++ updateLive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-(cd /home/justin/docker/datafortress.cloud.hugo ; hugo -d "/home/justin/docker/datafortress.cloud.hugo/public")
-git -C /home/justin/docker/datafortress.cloud.hugo/public add -A 
-git -C /home/justin/docker/datafortress.cloud.hugo/public commit -m "update to the live website"
-git -C /home/justin/docker/datafortress.cloud.hugo/public push origin master
-git -C /home/justin/docker/datafortress.cloud.hugo add -A
-git -C /home/justin/docker/datafortress.cloud.hugo commit -m "auto update to codebase"
-git -C /home/justin/docker/datafortress.cloud.hugo push origin master
+(cd /home/ubuntu/datafortress.cloud.hugo ; hugo -d "/home/ubuntu/datafortress.cloud.hugo/public")
+git -C /home/ubuntu/datafortress.cloud.hugo/public add -A 
+git -C /home/ubuntu/datafortress.cloud.hugo/public commit -m "update to the live website"
+git -C /home/ubuntu/datafortress.cloud.hugo/public push origin master
+git -C /home/ubuntu/datafortress.cloud.hugo add -A
+git -C /home/ubuntu/datafortress.cloud.hugo commit -m "auto update to codebase"
+git -C /home/ubuntu/datafortress.cloud.hugo push origin master
 echo "done"
