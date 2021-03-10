b7354805acdd53a48792955f01556ddfc0910123
Justin Guese <guese.justin@gmail.com>
2021-03-10 11:18:13 +0100

    auto update to codebase

--- updateLive-local.sh
+++ updateLive-local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-(cd /home/dflucy/Documents/python/datafortress.cloud.hugo ; hugo -d "/home/dflucy/Documents/python/datafortress.cloud.hugo/public")
-git -C /home/dflucy/Documents/python/datafortress.cloud.hugo/public add -A 
-git -C /home/dflucy/Documents/python/datafortress.cloud.hugo/public commit -m "update to the live website"
-git -C /home/dflucy/Documents/python/datafortress.cloud.hugo/public push origin master
-git -C /home/dflucy/Documents/python/datafortress.cloud.hugo add -A
-git -C /home/dflucy/Documents/python/datafortress.cloud.hugo commit -m "auto update to codebase"
-git -C /home/dflucy/Documents/python/datafortress.cloud.hugo push origin master
+(cd /home/justin/Documents/python/datafortress.cloud.hugo ; hugo -d "/home/justin/Documents/python/datafortress.cloud.hugo/public")
+git -C /home/justin/Documents/python/datafortress.cloud.hugo/public add -A 
+git -C /home/justin/Documents/python/datafortress.cloud.hugo/public commit -m "update to the live website"
+git -C /home/justin/Documents/python/datafortress.cloud.hugo/public push origin master
+git -C /home/justin/Documents/python/datafortress.cloud.hugo add -A
+git -C /home/justin/Documents/python/datafortress.cloud.hugo commit -m "auto update to codebase"
+git -C /home/justin/Documents/python/datafortress.cloud.hugo push origin master
 echo "done"
