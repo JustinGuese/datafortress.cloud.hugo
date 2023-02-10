61df0336a900a5bf036f18477f828325b429e78f
Justin Guese <guese.justin@gmail.com>
2023-02-10 17:27:42 +0100

    new blog post

--- public/rescursivePNG2Webp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-find . -name '*.png' -type f -exec bash -c 'cwebp -q 80 "$0" -o "${0%.png}.webp"' {} \;
-#
-find . -name '*.jpg' -type f -exec bash -c 'cwebp -q 80 "$0" -o "${0%.jpg}.webp"' {} \;
