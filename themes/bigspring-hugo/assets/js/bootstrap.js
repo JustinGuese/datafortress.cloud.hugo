e14ac995a7046139663db47269e934e0c9f61d78
Justin Guese <guese.justin@gmail.com>
2025-11-05 09:34:14 +0100

    bootstrap upgrade

--- themes/bigspring-hugo/assets/js/bootstrap.js
+++ themes/bigspring-hugo/assets/js/bootstrap.js
@@ -1,14 +1,10 @@
-// bootstrap js components
+// bootstrap js components (ESM from Hugo Bootstrap module)
 // import Alert from "js/bootstrap/src/alert";
 // import Button from "js/bootstrap/src/button";
 // import Carousel from "js/bootstrap/src/carousel";
-import Collapse from "js/bootstrap/src/collapse";
-import Dropdown from "js/bootstrap/src/dropdown";
-import Modal from "js/bootstrap/src/modal";
 // import Offcanvas from "js/bootstrap/src/offcanvas";
 // import Popover from "js/bootstrap/src/popover";
 // import ScrollSpy from "js/bootstrap/src/scrollspy";
-import Tab from "js/bootstrap/src/tab";
 // import Toast from "js/bootstrap/src/toast";
 // import Tooltip from "js/bootstrap/src/tooltip";
 
