975ecd1b3d6f460aac8a67ae1ff4342ee035da8f
Justin Guese <guese.justin@gmail.com>
2025-11-05 13:23:55 +0100

    hugo version upgrade

---
+++ _vendor/github.com/gethugothemes/hugo-modules/gallery-slider/assets/js/gallery-slider.js
@@ -0,0 +1,36 @@
+(function () {
+  "use strict";
+  // gallery init
+  GLightbox();
+
+  // justified gallery init
+  window.setTimeout(() => {
+    const justify_scale = screen.height * 0.25;
+    let items = document.querySelectorAll(".gallery-item");
+    Array.prototype.forEach.call(items, (item) => {
+      let image = item.querySelector("img");
+      let ratio = image.width / image.height;
+      item.style.width = justify_scale * ratio + "px";
+      item.style.flexGrow = ratio;
+    });
+  }, 200);
+
+  // gallery slider
+  var isGallerySlider = document.getElementsByClassName("gallery-slider");
+  if (isGallerySlider.length > 0) {
+    new Swiper(".gallery-slider", {
+      slidesPerView: 1,
+      loop: true,
+      autoHeight: true,
+      spaceBetween: 0,
+      speed: 1500,
+      autoplay: {
+        delay: 5000,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+})();
